ea8fd72da03c3054cd3b345be59eb580e5e50d63
pavel <gritsatsuev.p@promail366.com>
2023-11-11 18:37:05 +0400

    add crud project request with api platfrom

---
+++ src/Entity/Project.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[ApiResource]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $project_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProjectName(): ?string
+    {
+        return $this->project_name;
+    }
+
+    public function setProjectName(string $project_name): static
+    {
+        $this->project_name = $project_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
